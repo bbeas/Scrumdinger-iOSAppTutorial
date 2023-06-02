1fc6f4d54cfa6266c1c0f89363ad3601b6fa9cb4
bbeas <beibei_shen@outlook.com>
2023-06-02 21:52:34 +0100

    Update basic layout of MeetingView

---
+++ testApp/MeetingView.swift
@@ -0,0 +1,49 @@
+//
+//  ContentView.swift
+//  testApp
+//
+//  Created by Shen Beibei on 02/06/2023.
+//
+
+import SwiftUI
+
+// conforms to the View protocol and has a body property
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            ProgressView(value: 5, total: 15)
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Seconds Elapsed").font(.caption)
+                    Label("300", systemImage: "hourglass.tophalf.fill")
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Seconds Remaining").font(.caption)
+                    Label("600", systemImage: "hourglass.bottomhalf.fill")
+                }
+            }
+            .accessibilityElement(children: .ignore)
+            .accessibilityLabel("Time remaining")
+            .accessibilityValue("10 minutes")
+            Circle().strokeBorder(lineWidth: 24)
+            HStack{
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button(action: {}) {
+                    Image(systemName: "forward.fill")
+                }
+                .accessibilityLabel("Next speaker")
+            }
+        }
+        .padding()
+        
+    }
+}
+
+// defines a preview for that view to display
+struct MeetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView()
+    }
+}
