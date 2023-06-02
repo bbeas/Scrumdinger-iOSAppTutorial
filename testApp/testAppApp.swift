09db0926c2e9257669dbb481d361ce502238d824
bbeas <beibei_shen@outlook.com>
2023-06-02 20:16:14 +0100

    Initial Commit

---
+++ testApp/testAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  testAppApp.swift
+//  testApp
+//
+//  Created by Shen Beibei on 02/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct testAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
