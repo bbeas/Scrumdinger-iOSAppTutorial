df7b65465f8ca5932fecc2e6ebc690095375f30f
bbeas <beibei_shen@outlook.com>
2023-06-05 15:10:43 +0100

    Transcribing speech to text

--- testApp/Views/MeetingView.swift
+++ testApp/Views/MeetingView.swift
@@ -11,10 +11,15 @@ import SwiftUI
 // conforms to the View protocol and has a body property
 struct MeetingView: View {
     @Binding var scrum: DailyScrum
+
     // @StateObject means the view owns the source of truth for the obejct
     // It ties the Scrum Timer (ObservableObject) to the MeetingView lifecycle
     // to keep it alive for the lifecycle of the view
     @StateObject var scrumTimer = ScrumTimer()
+
+    @StateObject var speechRecognizer = SpeechRecognizer()
+
+    @State private var isRecording = false
 
     private var player: AVPlayer { AVPlayer.sharedDingPlayer }
 
@@ -24,7 +29,7 @@ struct MeetingView: View {
                 .fill(scrum.theme.mainColor)
             VStack {
                 MeetingHeaderView(secondsElapsed: scrumTimer.secondsElapsed, secondsRemaining: scrumTimer.secondsRemaining, theme: scrum.theme)
-                MeetingTimerView(speakers: scrumTimer.speakers, theme: scrum.theme)
+                MeetingTimerView(speakers: scrumTimer.speakers, isRecording: isRecording, theme: scrum.theme)
                 MeetingFooterView(speakers: scrumTimer.speakers, skipAction: scrumTimer.skipSpeaker)
             }
         }
@@ -46,12 +51,17 @@ struct MeetingView: View {
             player.seek(to: .zero)
             player.play()
         }
+        speechRecognizer.resetTranscript()
+        speechRecognizer.startTranscribing()
+        isRecording = true
         scrumTimer.startScrum()
     }
 
     private func endScrum() {
         scrumTimer.stopScrum()
-        let newHistory = History(attendees: scrum.attendees)
+        speechRecognizer.stopTranscribing()
+        isRecording = false
+        let newHistory = History(attendees: scrum.attendees, transcript: speechRecognizer.transcript)
         scrum.history.insert(newHistory, at: 0)
     }
 }
